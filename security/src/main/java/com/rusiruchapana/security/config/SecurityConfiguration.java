cb414c73a2fba589ea17f029683e0b9159322cc8
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-12 00:42:33 +0530

    role based authorization.

--- security/src/main/java/com/rusiruchapana/security/config/SecurityConfiguration.java
+++ security/src/main/java/com/rusiruchapana/security/config/SecurityConfiguration.java
@@ -1,9 +1,21 @@
 package com.rusiruchapana.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(registry->{
+                registry.requestMatchers("/home").permitAll();
+                registry.requestMatchers("/admin/").hasRole("ADMIN");
+                registry.requestMatchers("/user/").hasRole("USER");
+                registry.anyRequest().authenticated();
+        }).build();
+    }
+
 }
