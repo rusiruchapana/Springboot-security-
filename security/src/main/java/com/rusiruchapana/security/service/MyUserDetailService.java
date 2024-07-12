23e2599ef5daf0ce9b1b68b8a67e92c5503a1069
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-12 14:01:59 +0530

    Update MyUserDetailService.java

--- security/src/main/java/com/rusiruchapana/security/service/MyUserDetailService.java
+++ security/src/main/java/com/rusiruchapana/security/service/MyUserDetailService.java
@@ -1,10 +1,13 @@
 package com.rusiruchapana.security.service;
 
+import com.rusiruchapana.security.entity.MyUser;
 import com.rusiruchapana.security.repository.MyUserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class MyUserDetailService implements UserDetailsService {
@@ -14,9 +17,13 @@ public class MyUserDetailService implements UserDetailsService {
         this.myUserRepository = myUserRepository;
     }
 
-    
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<MyUser> myUser = myUserRepository.findByUsername(username);
+
+        
         return null;
     }
 }
