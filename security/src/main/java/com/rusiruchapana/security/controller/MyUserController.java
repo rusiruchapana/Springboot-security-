35e9cdd7f0636422a270c43100ea359027a8c79f
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-12 15:49:14 +0530

    Update MyUserController.java

--- security/src/main/java/com/rusiruchapana/security/controller/MyUserController.java
+++ security/src/main/java/com/rusiruchapana/security/controller/MyUserController.java
@@ -1,8 +1,16 @@
 package com.rusiruchapana.security.controller;
 
+import com.rusiruchapana.security.entity.MyUser;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyUserController {
+
+    @PostMapping("/save")
+    public MyUser register(@RequestBody MyUser myUser){
+        
+    }
 
 }
