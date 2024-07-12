45989f43f0437181575012c464fa2da5d361f811
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-12 12:00:43 +0530

    Update MyUser.java

--- security/src/main/java/com/rusiruchapana/security/entity/MyUser.java
+++ security/src/main/java/com/rusiruchapana/security/entity/MyUser.java
@@ -1,13 +1,13 @@
 package com.rusiruchapana.security.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "my_users")
 public class MyUser {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String username;
     private String password;
