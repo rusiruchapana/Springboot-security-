21841c8a2a84351c4cb2510a19e9be1f80cbdcd7
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-12 12:01:42 +0530

    lombok annotations.

--- security/src/main/java/com/rusiruchapana/security/entity/MyUser.java
+++ security/src/main/java/com/rusiruchapana/security/entity/MyUser.java
@@ -1,9 +1,17 @@
 package com.rusiruchapana.security.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "my_users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class MyUser {
 
     @Id
