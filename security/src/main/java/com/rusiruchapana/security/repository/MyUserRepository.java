5707962687b2d5c17ce0004849ddcc245ab57a5b
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-12 12:04:04 +0530

    Update MyUserRepository.java

--- security/src/main/java/com/rusiruchapana/security/repository/MyUserRepository.java
+++ security/src/main/java/com/rusiruchapana/security/repository/MyUserRepository.java
@@ -1,4 +1,7 @@
 package com.rusiruchapana.security.repository;
 
-public interface MyUserRepository {
+import com.rusiruchapana.security.entity.MyUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MyUserRepository extends JpaRepository<MyUser,Long> {
 }
